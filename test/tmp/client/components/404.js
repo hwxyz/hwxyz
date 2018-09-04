e3eed6acea8c221162406de2b9f18cd17779c6d3
huomarvin <huo.marvin@gmail.com>
2018-09-04 16:18:02 +0800

    rollback

--- test/tmp/client/components/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-    <div>
-        <h1>Sorry, can’t find that...</h1>
-    </div>
-);
-
-export default NotFound;
--- test/tmp/config/index.js
+++
@@ -1,7 +0,0 @@
-const ENV = {
-    default: require('./default.json'),
-    test: require('./test.json'),
-    prd: require('./prd.json')
-}[process.env.DEPLOY_ENV || 'default'];
-
-module.exports = ENV;
